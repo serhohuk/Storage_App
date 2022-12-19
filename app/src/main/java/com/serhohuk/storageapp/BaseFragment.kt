24a8c572730c3eb7c0460a2ad98d49c923bf4d91
Serh Hryhorchuk <gserey27@gmail.com>
2022-12-19 16:10:39 +0200

    clear unused files

--- app/src/main/java/com/serhohuk/storageapp/BaseFragment.kt
+++ app/src/main/java/com/serhohuk/storageapp/BaseFragment.kt
@@ -5,16 +5,8 @@ import androidx.fragment.app.FragmentManager
 
 open class BaseFragment : Fragment() {
 
-    fun navigateToOtOFragment(fm: FragmentManager) {
-        replaceFragmentWithBackStack(fm, OtOFragment())
-    }
-
     fun navigateToOtMFragment(fm: FragmentManager) {
         replaceFragmentWithBackStack(fm, OtMFragment())
-    }
-
-    fun navigateToMtMFragment(fm: FragmentManager) {
-        replaceFragmentWithBackStack(fm, MtMFragment())
     }
 
     private fun replaceFragmentWithBackStack(fm: FragmentManager, fragment: Fragment) {
